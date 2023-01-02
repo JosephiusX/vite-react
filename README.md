a35792a21b340af83a68db5e5908d8877f03ced2
josephius x <josephiusx@gmail.com>
2023-01-02 02:54:15 -0800

    Creating Contacts

---
+++ README.md
@@ -0,0 +1,4 @@
+https://github.com/remix-run/react-router/blob/main/docs/start/tutorial.md
+
+Next step:
+  URL Params in Loaders
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
