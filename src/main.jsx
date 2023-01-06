fa0ad20f2ec0f6ff65ea781fc2b51d9ff2d7f49e
josephius x <josephiusx@gmail.com>
2023-01-05 21:54:35 -0800

    Deleting Records

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import ErrorPage from "./error-page";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +26,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
