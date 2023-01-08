ed5ff87661a56e3cf2c1d6698f1f124fc0abd97a
josephius x <josephiusx@gmail.com>
2023-01-07 19:06:45 -0800

    Submitting Forms onChange

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,6 +5,7 @@ import {
   Form,
   redirect,
   useNavigation,
+  useSubmit
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 import { useEffect } from "react";
@@ -24,7 +25,9 @@ export async function loader({ request }) {
 export default function Root() {
   const { contacts , q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
+  // 
   useEffect(() => {
     document.getElementById("q").value = q;
   }, [q]);
@@ -42,6 +45,9 @@ export default function Root() {
               type="search"
               name="q"
               defaultValue={q}
+              onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
             />
             <div
               id="search-spinner"
